e4e797beac263d38b848939e64e4339bb0c8f8d7
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-08-21 23:59:54 -0300

    configurando banco de dados e rabimtMQ

---
+++ src/test/java/com/micro/spring/processamento/ProcessamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.spring.processamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
